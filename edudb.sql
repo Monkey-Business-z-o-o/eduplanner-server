cb5ed6fa4769b542627dd8c19877baae67b9799d
Cyprian Czerwinski <cyprian.czerwinski@gmail.com>
2024-12-12 17:09:12 +0100

    i ci ...20

--- edudb.sql
+++ edudb.sql
@@ -1,3 +1,10 @@
-CREATE DATABASE eduplanner;
-CREATE USER root WITH PASSWORD 'root123';
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'eduplanner'
+    ) THEN
+        CREATE DATABASE eduplanner;
+    END IF;
+END $$;
+
 GRANT ALL PRIVILEGES ON DATABASE eduplanner TO root;
