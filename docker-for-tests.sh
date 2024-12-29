7a3987e740a2e440bdefffd4fa74df91ae8d5b64
s27454 <115408564+AleksanderBastekPja@users.noreply.github.com>
2024-12-29 20:54:33 +0100

    Update docker-for-tests.sh

--- docker-for-tests.sh
+++ docker-for-tests.sh
@@ -1,3 +1,3 @@
 docker stop postgresEp 2>/dev/null
 docker rm postgresEp 2>/dev/null
-docker run --name postgresEp -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root123 -e POSTGRES_DB=eduplanner -p 5432:5432 -d postgres:15
+docker run --name postgresEp -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root123 -e POSTGRES_DB=edudb -p 5432:5432 -d postgres:15
